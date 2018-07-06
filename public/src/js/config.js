79c74d5a9241d9c3b72af913e8df4d23fdac6bd6
root <emmanuel.utrum@pm.me>
2018-07-06 00:28:56 +0000

    changed "Komodo" for "Utrum"

--- public/src/js/config.js
+++ public/src/js/config.js
@@ -5,7 +5,7 @@ angular.module('insight').config(function($routeProvider) {
   $routeProvider.
     when('/block/:blockHash', {
       templateUrl: 'views/block.html',
-      title: 'Komodo Block '
+      title: 'OOT Block '
     }).
     when('/block-index/:blockHeight', {
       controller: 'BlocksController',
@@ -17,7 +17,7 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/tx/:txId/:v_type?/:v_index?', {
       templateUrl: 'views/transaction.html',
-      title: 'Komodo Transaction '
+      title: 'OOT Transaction '
     }).
     when('/', {
       templateUrl: 'views/index.html',
@@ -25,15 +25,15 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/blocks', {
       templateUrl: 'views/block_list.html',
-      title: 'Komodo Blocks solved Today'
+      title: 'OOT Blocks solved Today'
     }).
     when('/blocks-date/:blockDate/:startTimestamp?', {
       templateUrl: 'views/block_list.html',
-      title: 'Komodo Blocks solved '
+      title: 'OOT Blocks solved '
     }).
     when('/address/:addrStr', {
       templateUrl: 'views/address.html',
-      title: 'Komodo Address '
+      title: 'Utrum Address '
     }).
     when('/charts/:chartType?', {
       templateUrl: 'views/charts.html',
